ce47c3ce33af04a8a020bd35311c8dc401cca8d9
Tobiky <12644606+Tobiky@users.noreply.github.com>
2023-02-04 20:55:53 +0100

    fn + matches, find_match; impl Iterator for Matches

--- src/types.rs
+++ src/types.rs
@@ -1,9 +1,23 @@
 pub trait RegExp {
     fn is_match(&self, input: &str) -> bool;
-    fn matches(&self, input: &str) -> Vec<(usize, usize)>;
+    fn matches(&self, input: &str) -> Matches<Self>;
+    fn find_match(&self, input: &str) -> Option<(usize, usize)>;
 }
 
-pub struct Matches<'input_lifetime> {
+pub struct Matches<'input_lifetime, R: RegExp> {
     pos: usize,
     inp: &'input_lifetime str,
+    reg: R,
+}
+
+impl<'t, R: RegExp> Iterator for Matches<'t, R> {
+    type Item = (usize, usize);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.reg.find_match(self[self.pos..])
+            .map(|x| {
+                self.pos += x.1;
+                x
+            })
+    }
 }
