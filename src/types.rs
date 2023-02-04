67952cac950588fce0a4c8820d8a269b39f879f8
Tobiky <12644606+Tobiky@users.noreply.github.com>
2023-02-05 00:58:41 +0100

    fixed type issue, new iterator implementation

--- src/types.rs
+++ src/types.rs
@@ -1,7 +1,12 @@
-pub trait RegExp {
+pub trait RegExp
+where Self: Sized
+{
     fn is_match(&self, input: &str) -> bool;
     fn matches(&self, input: &str) -> Matches<Self>;
     fn find_match(&self, input: &str) -> Option<(usize, usize)>;
+    fn find_match_at(&self, input: &str, offset: usize) -> Option<(usize, usize)>;
+
+    const MIN_LEN: usize;
 }
 
 pub struct Matches<'input_lifetime, R: RegExp> {
@@ -14,10 +19,25 @@ impl<'t, R: RegExp> Iterator for Matches<'t, R> {
     type Item = (usize, usize);
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.reg.find_match(self[self.pos..])
-            .map(|x| {
-                self.pos += x.1;
-                x
-            })
+        // Boundry checking as well as length optimization
+        // Cannot match a (sub)string that has a length shorter
+        // than the minimum constructable length from the regex
+        while self.pos + R::MIN_LEN < self.inp.len() {
+            // Find the next matching location that starts exactly at position
+            let location_match = self.reg.find_match_at(self.inp, self.pos);
+
+            // Found a matching location, return it
+            if let Some(range) = location_match {
+                // Since a match was found the position needs to be updated to the
+                // ending position of the match since matches are not overlapping.
+                self.pos = range.1;
+                return Some(range)
+            } else {
+                // Increment along the search string since there was not match starting
+                // at this position
+                self.pos += 1;
+            }
+        }
+        None
     }
 }
