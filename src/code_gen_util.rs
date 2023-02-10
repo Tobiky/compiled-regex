85db3eb2632fa82eb5b3ed34834740c0106c3973
Tobiky <12644606+Tobiky@users.noreply.github.com>
2023-02-10 15:09:43 +0100

    added macro sha256str, regex_struct_name
    sha256str takes an expression and returns the sha256 hex string back.
    Expression must be AsRef<[u8]>.
    
    regex_struct_name takes an expression and returns the name for that
    regex subexpression, which is the leftmost half of a sha256 hex string
    prefix with two double scores. The naming is to avoid potential clashes
    as well as being able to reuse expressions.

---
+++ src/code_gen_util.rs
@@ -0,0 +1,25 @@
+macro_rules! sha256str {
+    ($str:expr) => {
+        {
+            use sha2::{Sha256, Digest};
+
+            let mut hasher = Sha256::new();
+            hasher.update($str);
+            let result = hasher.finalize();
+
+            hex::encode(result)
+        }
+    };
+}
+
+macro_rules! regex_struct_name {
+    ($rgx:expr) => {
+        {
+            let mut hex = sha256str!($rgx);
+            // Remove if clashing happens
+            hex.truncate(hex.len() / 2);
+            hex.insert_str(0, "__");
+            hex
+        }
+    };
+}
