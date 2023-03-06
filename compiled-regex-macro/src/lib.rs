37d89f5a1ac83a46aae9946250bd653ed7172578
Tobiky <12644606+Tobiky@users.noreply.github.com>
2023-03-06 09:36:08 +0100

    forgotten additions for last commit

--- compiled-regex-macro/src/lib.rs
+++ compiled-regex-macro/src/lib.rs
@@ -2,10 +2,13 @@ use compiled_regex_core::regex_syntax::ast::parse::Parser;
 use compiled_regex_core::ir;
 use compiled_regex_core::ir::IR;
 
+use compiled_regex_core::types::CompileError;
 use proc_macro::{self, TokenTree};
 use proc_macro::TokenStream;
 
-fn parse_regex_string(regex: &str) -> String {
+use litrs::StringLit;
+
+fn parse_regex_string(export_name: &str, regex: &str) -> String {
     // Try to parse the regex
     match Parser::new().parse(regex) {
         Ok(ast) => {
@@ -26,8 +29,9 @@ fn parse_regex_string(regex: &str) -> String {
 
                     // Temporary anchor for the generated final type
                     let code = format!(
-                        "{}\ntype Regex = __m{}::{};",
+                        "{}\ntype {} = __m{}::{};",
                         code,
+                        export_name,
                         name,
                         name);
 
@@ -50,14 +54,68 @@ fn parse_regex_string(regex: &str) -> String {
     }
 }
 
+fn parse_token_stream(tokens: TokenStream) -> Result<(String, String), CompileError> {
+    if tokens.is_empty() {
+        // TODO: Should return error describing empty token set
+        return Err(CompileError::UnexpectedToken(0, 0))
+    }
+
+    let mut iter = tokens.into_iter();
+
+    // Find the name (an identity or string literal) for the RegEx
+    let name = match iter.next() {
+        Some(TokenTree::Ident(x)) => x.to_string(),
+        Some(TokenTree::Literal(x)) => {
+            if let Ok(s) = StringLit::try_from(x) {
+                s.to_string()
+            } else {
+                // TODO: Specify illegal literal type usage
+                return Err(CompileError::UnexpectedToken(0, 0))
+            }
+        }
+        // TODO: Specifiy that identity or name is needed
+        _ => return Err(CompileError::UnexpectedToken(0, 0))
+    };
+
+    // Make sure there is a delimiter inbetween
+    // Can possibly skip this by just making sure that the the last
+    // item is correct
+    if !matches!(iter.next(), Some(TokenTree::Punct(_))) {
+        // TODO: Specify that some punctuation is needed
+        // currently can be any, not sure if it should be forced
+        // to anything specific.
+        return Err(CompileError::UnexpectedToken(0, 0))
+    }
+
+    // Get the regex string literal
+    let regex = match iter.next() {
+        Some(TokenTree::Literal(x)) => {
+            if let Ok(s) = StringLit::try_from(x) {
+                s.to_string()
+            } else {
+                // TODO: Specify illegal literal type usage
+                return Err(CompileError::UnexpectedToken(0, 0))
+            }
+        }
+        // TODO: Specifiy that str literal is needed
+        _ => return Err(CompileError::UnexpectedToken(0, 0))
+    };
+
+    // Strip surrounding string marks from RegEx literal
+    let regex = regex.as_str()[regex.find('"').unwrap() + 1..regex.rfind('"').unwrap()].to_string();
+
+    Ok((name, regex))
+}
+
 #[proc_macro]
 pub fn parse_regex(tokens: TokenStream) -> TokenStream {
-    // Get the regex within the quotes
-    let inp = tokens.to_string();
-    let inp = inp[inp.find('"').unwrap() + 1..inp.rfind('"').unwrap()].to_string();
+    // Parse the tokens into a name and a RegEx literal
+    // TODO: CompileError report
+    let (name, regex) = parse_token_stream(tokens).unwrap();
+    // format!(r###"println!("{{}}", r##"{:?}"##)"###, (name, regex)).parse().unwrap()
 
     // Parse the RegEx into actual code
-    let code = parse_regex_string(&inp);
+    let code = parse_regex_string(&name, &regex);
 
     // Parse the code into Rust tokens
     return code.parse().unwrap()
